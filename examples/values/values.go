395be7139e2058a4d5089ce138a6eb3a737d1600
mingrammer <k239507@gmail.com>
2017-01-06 03:09:41 +0900

    Translate the values example

--- examples/values/values.go
+++ examples/values/values.go
@@ -1,21 +1,19 @@
-// Go has various value types including strings,
-// integers, floats, booleans, etc. Here are a few
-// basic examples.
-
+// Go에는 문자열, 정수, 실수, 불리언등을 포함한 다양한 타입들이 존재합니다.
+//  여기에 몇 가지 기본적인 예시들이 있습니다.
 package main
 
 import "fmt"
 
 func main() {
 
-	// Strings, which can be added together with `+`.
+	// 문자열은 `+`로 합칠 수 있습니다.
 	fmt.Println("go" + "lang")
 
-	// Integers and floats.
+	// 정수와 실수 예시
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-	// Booleans, with boolean operators as you'd expect.
+	// 불리언값들은 여러분들이 예상하는대로 동작합니다.
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)

5973fedc92d866feb56c669d62b09265da345ace
mingrammer <k239507@gmail.com>
2017-01-05 18:48:20 +0900

    Translate some common untranslated text

--- examples/hello-world/hello-world.go
+++ examples/hello-world/hello-world.go
@@ -1,5 +1,5 @@
-// Our first program will print the classic "hello world"
-// message. Here's the full source code.
+// 우리의 첫번째 프로그램은 고전적인 "hello world" 메시지를 출력하는 프로그램입니다.
+//  여기에 전체 소스 코드가 있습니다.
 package main
 
 import "fmt"
--- examples/hello-world/hello-world.sh
+++ examples/hello-world/hello-world.sh
@@ -1,17 +1,15 @@
-# To run the program, put the code in `hello-world.go` and
-# use `go run`.
+# 프로그램을 실행하기 위해, `hello-world.go`에 코드를 작성한 뒤 `go run`을 실행합니다.
 $ go run hello-world.go
 hello world
 
-# Sometimes we'll want to build our programs into
-# binaries. We can do this using `go build`.
+# 가끔은 프로그램을 바이너리로 빌드하고 싶을때가 있습니다. 이 땐 `go build`를 사용할 수 있습니다.
 $ go build hello-world.go
 $ ls
 hello-world	hello-world.go
 
 # We can then execute the built binary directly.
+# 그런 다음 빌드된 바이너리를 직접 실행할 수 있습니다.
 $ ./hello-world
 hello world
 
-# Now that we can run and build basic Go programs, let's
-# learn more about the language.
+# 이제 우리는 Go 프로그램을 실행하고 빌드할 수 있습니다, Go 언어에 대해 좀 더 배워봅시다.

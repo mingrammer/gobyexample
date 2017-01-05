b4c49ed28b71097a240ee3951e7e9a4ae456f956
mingrammer <k239507@gmail.com>
2017-01-06 04:36:42 +0900

    Translate the variables example

--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -1,6 +1,5 @@
-// In Go, _variables_ are explicitly declared and used by
-// the compiler to e.g. check type-correctness of function
-// calls.
+// Go에서, _변수(variables)_는 명시적으로 선언되며 컴파일러에 의해 사용됩니다.
+//  예를 들면 함수 호출에서 타입이 정확한지 검사하는데 사용됩니다.
 
 package main
 
@@ -8,27 +7,25 @@ import "fmt"
 
 func main() {
 
-	// `var` declares 1 or more variables.
+	// `var`는 하나 또는 여러개의 변수를 선언합니다.
 	var a string = "initial"
 	fmt.Println(a)
 
-	// You can declare multiple variables at once.
+	// 한 번에 여러개의 변수를 선언할 수 있습니다.
 	var b, c int = 1, 2
 	fmt.Println(b, c)
 
-	// Go will infer the type of initialized variables.
+	// Go는 초기화된 변수의 타입을 추론합니다.
 	var d = true
 	fmt.Println(d)
 
-	// Variables declared without a corresponding
-	// initialization are _zero-valued_. For example, the
-	// zero value for an `int` is `0`.
+	// 초기화 없이 선언된 변수는 제로값을 갖게 됩니다.
+	//  예를 들면, `int`의 제로값은 `0`입니다.
 	var e int
 	fmt.Println(e)
 
-	// The `:=` syntax is shorthand for declaring and
-	// initializing a variable, e.g. for
-	// `var f string = "short"` in this case.
+	// `:=` 문법은 변수를 선언하는 동시에 초기화하기 위한 단축 표현식입니다.
+	//  예를 들면 이 경우는 `var f string = "short"`를 뜻합니다.
 	f := "short"
 	fmt.Println(f)
 }

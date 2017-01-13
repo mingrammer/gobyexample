998d7d9a01eff0b9014487c3724e7f35e5c0b16c
mingrammer <k239507@gmail.com>
2017-01-13 19:18:20 +0900

    Translate the maps

--- examples/maps/maps.go
+++ examples/maps/maps.go
@@ -1,50 +1,39 @@
-// _Maps_ are Go's built-in [associative data type](http://en.wikipedia.org/wiki/Associative_array)
-// (sometimes called _hashes_ or _dicts_ in other languages).
-
+// _Maps_는 Go의 내장 [연관 데이터 타입(associative data type)](http://en.wikipedia.org/wiki/Associative_array) 입니다.
+//  (다른 언어에선 _hashes_ 또는 _dicts_라고 부르기도 합니다.)
 package main
 
 import "fmt"
 
 func main() {
 
-	// To create an empty map, use the builtin `make`:
-	// `make(map[key-type]val-type)`.
+	// 빈 맵을 생성하기 위해 내장 함수인 `make`를 사용합니다 : `make(map[key-type]val-type)`
 	m := make(map[string]int)
 
-	// Set key/value pairs using typical `name[key] = val`
-	// syntax.
+	// `name[key] = val`로 key/value 쌍을 저장합니다.
 	m["k1"] = 7
 	m["k2"] = 13
 
-	// Printing a map with e.g. `Println` will show all of
-	// its key/value pairs.
+	// 맵 출력. `Println`는 맵의 모든 key/value 쌍들을 출력합니다.
 	fmt.Println("map:", m)
 
-	// Get a value for a key with `name[key]`.
+	// `name[key]`로 키에 해당하는 값을 가져옵니다.
 	v1 := m["k1"]
 	fmt.Println("v1: ", v1)
 
-	// The builtin `len` returns the number of key/value
-	// pairs when called on a map.
+	// `len`을 맵에 사용하면 key/value 쌍의 갯수를 반환합니다.
 	fmt.Println("len:", len(m))
 
-	// The builtin `delete` removes key/value pairs from
-	// a map.
+	// `delete` 내장 함수는 맵의 key/value 쌍을 삭제합니다.
 	delete(m, "k2")
 	fmt.Println("map:", m)
 
-	// The optional second return value when getting a
-	// value from a map indicates if the key was present
-	// in the map. This can be used to disambiguate
-	// between missing keys and keys with zero values
-	// like `0` or `""`. Here we didn't need the value
-	// itself, so we ignored it with the _blank identifier_
-	// `_`.
+	// 맵에서 값을 꺼내오면서 반환되는 선택적인 두번째 반환값은 해당 키가 맵에 존재하는지에 대한 여부를 나타냅니다.
+	//  이는 키값이 존재하지 않는건지 또는 해당 값이 `0`이나 `""`과 같은 제로값 인지를 명확하게 구분하는데 사용할 수 있습니다.
+	//  값 자체가 필요없는 경우엔, _공백 식별자(blank identifier)_ `_`를 사용해 무시할 수 있습니다.
 	_, prs := m["k2"]
 	fmt.Println("prs:", prs)
 
-	// You can also declare and initialize a new map in
-	// the same line with this syntax.
+	// 또한 한 줄로 맵 선언 및 초기화를 할 수도 있습니다.
 	n := map[string]int{"foo": 1, "bar": 2}
 	fmt.Println("map:", n)
 }
--- examples/maps/maps.sh
+++ examples/maps/maps.sh
@@ -1,6 +1,5 @@
-# Note that maps appear in the form `map[k:v k:v]` when
-# printed with `fmt.Println`.
-$ go run maps.go 
+# 참고로 맵을 `fmt.Println`으로 출력하면 `map[k:v k:v]`의 형태로 출력됩니다.
+$ go run maps.go
 map: map[k1:7 k2:13]
 v1:  7
 len: 2

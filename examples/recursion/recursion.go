bf37c29fe2731eb991bede96be17010a0c9a687e
mingrammer <k239507@gmail.com>
2017-01-14 21:34:58 +0900

    Translate the recursion

--- examples/recursion/recursion.go
+++ examples/recursion/recursion.go
@@ -1,13 +1,11 @@
-// Go supports
-// <a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>.
-// Here's a classic factorial example.
+// Go는 <a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>를 지원합니다.
+//  여기에 고전적인 팩토리얼 예시가 있습니다.
 
 package main
 
 import "fmt"
 
-// This `fact` function calls itself until it reaches the
-// base case of `fact(0)`.
+// `fact` 함수는 베이스 케이스인 `fact(0)`에 도달할 때까지 자기자신을 호출합니다.
 func fact(n int) int {
 	if n == 0 {
 		return 1

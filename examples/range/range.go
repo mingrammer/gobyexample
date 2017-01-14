ee931726d93108c3a85f219e3a18e2a7da87cdeb
mingrammer <k239507@gmail.com>
2017-01-14 20:05:29 +0900

    Translate the range

--- examples/range/range.go
+++ examples/range/range.go
@@ -1,6 +1,5 @@
-// _range_ iterates over elements in a variety of data
-// structures. Let's see how to use `range` with some
-// of the data structures we've already learned.
+// _range_는 다양한 데이터 구조의 요소들을 순회합니다.
+//  우리가 이미 배운 몇 가지 데이터 구조와 함께 `range`를 사용하는 방법을 살펴봅시다.
 
 package main
 
@@ -8,8 +7,8 @@ import "fmt"
 
 func main() {
 
-	// Here we use `range` to sum the numbers in a slice.
-	// Arrays work like this too.
+	// 다음 예시는 슬라이스에 있는 숫자들을 더하기 위해 `range`를 사용합니다.
+	//  배열에서도 똑같이 동작합니다.
 	nums := []int{2, 3, 4}
 	sum := 0
 	for _, num := range nums {
@@ -17,31 +16,28 @@ func main() {
 	}
 	fmt.Println("sum:", sum)
 
-	// `range` on arrays and slices provides both the
-	// index and value for each entry. Above we didn't
-	// need the index, so we ignored it with the
-	// blank identifier `_`. Sometimes we actually want
-	// the indexes though.
+	// `range`를 배열과 슬라이스에서 사용하면 각 원소의 인덱스와 값을 반환합니다.
+	//  위 예시에선 인덱스가 필요 없었기 때문에 공백 식별자 `_`로 인덱스를 무시했습니다.
+	//  다음 예시처럼 인덱스가 필요할 때도 있습니다.
 	for i, num := range nums {
 		if num == 3 {
 			fmt.Println("index:", i)
 		}
 	}
 
-	// `range` on map iterates over key/value pairs.
+	// 맵에서 `range`는 key/value 쌍들을 순회합니다.
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
-	// `range` can also iterate over just the keys of a map.
+	// `range`로 map의 키값들만 순회할 수도 있습니다.
 	for k := range kvs {
 		fmt.Println("key:", k)
 	}
 
-	// `range` on strings iterates over Unicode code
-	// points. The first value is the starting byte index
-	// of the `rune` and the second the `rune` itself.
+	// 문자열에서 `range`는 유니코드 코드 포인트들을 순회합니다.
+	//  첫번째 값은 `rune`의 시작 바이트 위치이며 두번째 값은 `rune`값 자체입니다.
 	for i, c := range "go" {
 		fmt.Println(i, c)
 	}

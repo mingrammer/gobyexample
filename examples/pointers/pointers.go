2d9e3ae9cd04cdbfc2deef5c985bf81b0bcbc7b1
mingrammer <k239507@gmail.com>
2017-01-17 18:49:54 +0900

    Translate the pointers

--- examples/pointers/pointers.go
+++ examples/pointers/pointers.go
@@ -1,26 +1,19 @@
-// Go supports <em><a href="http://en.wikipedia.org/wiki/Pointer_(computer_programming)">pointers</a></em>,
-// allowing you to pass references to values and records
-// within your program.
+// Go는 프로그램내에서 값의 참조와 레코드를 전달할 수 있는 <em><a href="http://en.wikipedia.org/wiki/Pointer_(computer_programming)">pointers</a></em>를 지원합니다.
 
 package main
 
 import "fmt"
 
-// We'll show how pointers work in contrast to values with
-// 2 functions: `zeroval` and `zeroptr`. `zeroval` has an
-// `int` parameter, so arguments will be passed to it by
-// value. `zeroval` will get a copy of `ival` distinct
-// from the one in the calling function.
+// 두 함수 `zeroval`와 `zeroptr`를 가지고 포인터가 값과 다르게 어떻게 동작하는지 살펴봅시다.
+//  `zeroval`은 `int` 타입 파라미터를 가지므로 인자는 값으로 전달됩니다.
+//  `zeroval`는 함수를 호출할 때의 값을 `ival`에 복사하여 가져옵니다.
 func zeroval(ival int) {
 	ival = 0
 }
 
-// `zeroptr` in contrast has an `*int` parameter, meaning
-// that it takes an `int` pointer. The `*iptr` code in the
-// function body then _dereferences_ the pointer from its
-// memory address to the current value at that address.
-// Assigning a value to a dereferenced pointer changes the
-// value at the referenced address.
+// 그에 반해 `zeroptr`는 `int`형 포인터를 받도록 `*int` 타입을 파라미터로 갖고 있습니다.
+//  함수안에서 `*iptr`는 메모리 주소에서 해당 주소의 현재값으로 포인터를 _역참조(dereference)_ 합니다.
+//  역참조된 포인터에 값을 할당하면 이는 참조된 주소의 값을 바꿉니다.
 func zeroptr(iptr *int) {
 	*iptr = 0
 }
@@ -32,11 +25,11 @@ func main() {
 	zeroval(i)
 	fmt.Println("zeroval:", i)
 
-	// The `&i` syntax gives the memory address of `i`,
-	// i.e. a pointer to `i`.
+	// `&i`는 `i`의 메모리 주소를 반환합니다.
+	// 즉, `i`의 포인터입니다.
 	zeroptr(&i)
 	fmt.Println("zeroptr:", i)
 
-	// Pointers can be printed too.
+	// 포인터도 출력할 수 있습니다.
 	fmt.Println("pointer:", &i)
 }
--- examples/pointers/pointers.sh
+++ examples/pointers/pointers.sh
@@ -1,6 +1,5 @@
-# `zeroval` doesn't change the `i` in `main`, but
-# `zeroptr` does because it has a reference to
-# the memory address for that variable.
+# `zeroval`은 `main`에 있는 `i`값을 바꾸지 않지만,
+# `zeroptr`은 해당 값의 메모리 주소를 참조하고 있기 때문에 값을 바꿀 수 있습니다.
 $ go run pointers.go
 initial: 1
 zeroval: 1

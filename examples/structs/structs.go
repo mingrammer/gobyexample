dcfd3448fde03a6440abe7910c199120d871daeb
mingrammer <k239507@gmail.com>
2017-01-17 18:59:09 +0900

    Translate the structs

--- examples/structs/structs.go
+++ examples/structs/structs.go
@@ -1,12 +1,11 @@
-// Go's _structs_ are typed collections of fields.
-// They're useful for grouping data together to form
-// records.
+// Go의 _structs_는 필드들로 이루어진 타입을 갖는 컬렉션입니다.
+//  레코드를 구성하기 위해 데이터들을 그룹핑 하는데 유용합니다.
 
 package main
 
 import "fmt"
 
-// This `person` struct type has `name` and `age` fields.
+// 이 `person` 구조체 타입은 `name`과 `age` 필드를 갖습니다.
 type person struct {
 	name string
 	age  int
@@ -14,28 +13,28 @@ type person struct {
 
 func main() {
 
-	// This syntax creates a new struct.
+	// 새로운 구조체를 생성합니다.
 	fmt.Println(person{"Bob", 20})
 
-	// You can name the fields when initializing a struct.
+	// 필드명을 사용해 구조체를 초기화 할 수 있습니다.
 	fmt.Println(person{name: "Alice", age: 30})
 
-	// Omitted fields will be zero-valued.
+	// 생략된 필드는 제로값을 갖게됩니다.
 	fmt.Println(person{name: "Fred"})
 
-	// An `&` prefix yields a pointer to the struct.
+	// 앞에 `&`를 붙이면 구조체 포인터를 생성할 수 있습니다.
 	fmt.Println(&person{name: "Ann", age: 40})
 
-	// Access struct fields with a dot.
+	// 닷(.)을 사용해 구조체 필드에 접근합니다.
 	s := person{name: "Sean", age: 50}
 	fmt.Println(s.name)
 
-	// You can also use dots with struct pointers - the
-	// pointers are automatically dereferenced.
+	// 구조체 포인터에서도 닷(.)을 사용할 수 있습니다.
+	//  이 때 포인터는 자동으로 역참조됩니다.
 	sp := &s
 	fmt.Println(sp.age)
 
-	// Structs are mutable.
+	// 구조체는 수정이 가능(mutable)합니다.
 	sp.age = 51
 	fmt.Println(sp.age)
 }

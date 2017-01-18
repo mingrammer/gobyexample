dc8d0ff00dbeb9acdfadbcccc53fe8198b6470fd
mingrammer <k239507@gmail.com>
2017-01-18 17:19:01 +0900

    Translate the methods

--- examples/methods/methods.go
+++ examples/methods/methods.go
@@ -1,4 +1,4 @@
-// Go supports _methods_ defined on struct types.
+// Go는 구조체 타입에 정의할 수 있는 _methods_를 지원합니다.
 
 package main
 
@@ -8,13 +8,13 @@ type rect struct {
 	width, height int
 }
 
-// This `area` method has a _receiver type_ of `*rect`.
+// `area` 메서드는 `*rect`를 _리시버 타입(receiver type)_으로 갖습니다.
 func (r *rect) area() int {
 	return r.width * r.height
 }
 
-// Methods can be defined for either pointer or value
-// receiver types. Here's an example of a value receiver.
+// 메서드는 포인터 또는 값을 리시버 타입으로하여 정의될 수 있습니다.
+//  여기에 값을 리시버로 하는 예시가 있습니다.
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
 }
@@ -22,15 +22,12 @@ func (r rect) perim() int {
 func main() {
 	r := rect{width: 10, height: 5}
 
-	// Here we call the 2 methods defined for our struct.
+	// 구조체에 정의된 두 개의 메서드를 호출했습니다.
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim:", r.perim())
 
-	// Go automatically handles conversion between values
-	// and pointers for method calls. You may want to use
-	// a pointer receiver type to avoid copying on method
-	// calls or to allow the method to mutate the
-	// receiving struct.
+	// Go는 메서드 호출에 대해 값과 포인터간의 변환을 자동으로 처리합니다.
+	//  메서드 호출시 값 복사를 피하기 위해 포인터 리시버 타입을 사용할 수도 있고 전달되는 구조체를 변경할 수 있도록 할 수도 있습니다.
 	rp := &r
 	fmt.Println("area: ", rp.area())
 	fmt.Println("perim:", rp.perim())
--- examples/methods/methods.sh
+++ examples/methods/methods.sh
@@ -4,5 +4,4 @@ perim: 30
 area:  50
 perim: 30
 
-# Next we'll look at Go's mechanism for grouping and
-# naming related sets of methods: interfaces.
+# 다음 장에선 Go에서 관련있는 메서드들을 그룹핑하고 네이밍 하기위한 메커니즘을 살펴봅시다: 인터페이스 (interfaces).

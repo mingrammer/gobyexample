d0d7bc66a7cba9a9e70990e54b1b0218fc4176ff
mingrammer <k239507@gmail.com>
2017-01-20 17:36:53 +0900

    Translate the select

--- examples/select/select.go
+++ examples/select/select.go
@@ -1,6 +1,5 @@
-// Go's _select_ lets you wait on multiple channel
-// operations. Combining goroutines and channels with
-// select is a powerful feature of Go.
+// Go의 _select_는 다중 채널 연산들을 대기할 수 있도록 해줍니다.
+//  select을 사용한 고루틴과 채널의 조합은 Go의 강력한 기능입니다.
 
 package main
 
@@ -9,13 +8,11 @@ import "fmt"
 
 func main() {
 
-	// For our example we'll select across two channels.
+	// 이 예제에서 우리는 두 채널에서 select를 사용하겠습니다.
 	c1 := make(chan string)
 	c2 := make(chan string)
 
-	// Each channel will receive a value after some amount
-	// of time, to simulate e.g. blocking RPC operations
-	// executing in concurrent goroutines.
+	// 각 채널은 한 가지 예로 동시에 실행되는 고루틴에서의 RPC 연산의 실행을 블로킹하는 경우를 시뮬레이션 하기위해 일정 시간 후에 값을 받습니다.
 	go func() {
 		time.Sleep(time.Second * 1)
 		c1 <- "one"
@@ -25,8 +22,7 @@ func main() {
 		c2 <- "two"
 	}()
 
-	// We'll use `select` to await both of these values
-	// simultaneously, printing each one as it arrives.
+	// `select`를 사용하여 동시에 이 값들을 대기하며 도착하는대로 각 값을 출력합니다.
 	for i := 0; i < 2; i++ {
 		select {
 		case msg1 := <-c1:
--- examples/select/select.sh
+++ examples/select/select.sh
@@ -1,10 +1,7 @@
-# We receive the values `"one"` and then `"two"` as
-# expected.
+# 예상대로 `"one"`을 받은 후 `"two"`를 받습니다.
 $ time go run select.go 
 received one
 received two
 
-# Note that the total execution time is only ~2 seconds
-# since both the 1 and 2 second `Sleeps` execute
-# concurrently.
+# 참고로 총 실행 시간은 2초 정도밖에 안되는데 1초와 2초 `Sleeps`이 동시에 실행되기 때문입니다.
 real	0m2.245s

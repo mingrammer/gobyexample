1f5ceb20f99e4a8f7feb7b1b2b785104b5b4834f
mingrammer <k239507@gmail.com>
2017-01-20 21:14:40 +0900

    Translate the tickers

--- examples/tickers/tickers.go
+++ examples/tickers/tickers.go
@@ -1,8 +1,6 @@
-// [Timers](timers) are for when you want to do
-// something once in the future - _tickers_ are for when
-// you want to do something repeatedly at regular
-// intervals. Here's an example of a ticker that ticks
-// periodically until we stop it.
+// [Timers](/gobyexample/timers)는 미래의 한 시점에 무언가를 하고싶을때 사용할 수 있습니다.
+//  _tickers_는 일정한 간격으로 무언가를 반복하고자 할 때 사용할 수 있습니다.
+//  여기에 우리가 중단할 때까지 주기적으로 작업을 반복하는 티커의 예시가 있습니다.
 
 package main
 
@@ -11,10 +9,8 @@ import "fmt"
 
 func main() {
 
-	// Tickers use a similar mechanism to timers: a
-	// channel that is sent values. Here we'll use the
-	// `range` builtin on the channel to iterate over
-	// the values as they arrive every 500ms.
+	// 티커는 타이머와 유사한 메커니즘을 사용합니다: 값을 전송하는 채널을 사용.
+	//  다음은 매 500ms마다 도착하는 값들을 순회하기 위해 채널에서 `range`를 사용합니다.
 	ticker := time.NewTicker(time.Millisecond * 500)
 	go func() {
 		for t := range ticker.C {
@@ -22,9 +18,8 @@ func main() {
 		}
 	}()
 
-	// Tickers can be stopped like timers. Once a ticker
-	// is stopped it won't receive any more values on its
-	// channel. We'll stop ours after 1600ms.
+	// 티커는 타이머와 유사한 방식으로 중단될 수 있습니다.
+	//  티커가 멈추면 티커의 채널은 더 이상 값들을 수신하지 않습니다. 다음은 티커를 1600ms 후에 중단시킵니다.
 	time.Sleep(time.Millisecond * 1600)
 	ticker.Stop()
 	fmt.Println("Ticker stopped")
--- examples/tickers/tickers.sh
+++ examples/tickers/tickers.sh
@@ -1,5 +1,4 @@
-# When we run this program the ticker should tick 3 times
-# before we stop it.
+# 이 프로그램을 실행하면 티커는 프로그램이 종료되기 전에 3번의 틱을 발생시켜야합니다
 $ go run tickers.go
 Tick at 2012-09-23 11:29:56.487625 -0700 PDT
 Tick at 2012-09-23 11:29:56.988063 -0700 PDT

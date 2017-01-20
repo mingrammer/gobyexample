be468d091ec1f0e034b00f39554c59d19d293f28
mingrammer <k239507@gmail.com>
2017-01-20 19:10:16 +0900

    Translate the range-over-channels

--- examples/range-over-channels/range-over-channels.go
+++ examples/range-over-channels/range-over-channels.go
@@ -1,7 +1,4 @@
-// In a [previous](range) example we saw how `for` and
-// `range` provide iteration over basic data structures.
-// We can also use this syntax to iterate over
-// values received from a channel.
+// [이전](/gobyexample/range) 예제에서 우리는 `for`와 `range`가 기본 자료구조에서 어떻게 순회하는지 살펴보았습니다.
 
 package main
 
@@ -9,16 +6,14 @@ import "fmt"
 
 func main() {
 
-	// We'll iterate over 2 values in the `queue` channel.
+	// `queue` 채널에 있는 2개의 값을 순회 해보겠습니다.
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
 	close(queue)
 
-	// This `range` iterates over each element as it's
-	// received from `queue`. Because we `close`d the
-	// channel above, the iteration terminates after
-	// receiving the 2 elements.
+	// 이 `range`는 `queue`로부터 수신된 각 원소들을 순회합니다.
+	//  위에서 채널을 `종료(close)`했기 때문에 이 순회 루프는 2개의 원소를 받으면 종료됩니다.
 	for elem := range queue {
 		fmt.Println(elem)
 	}
--- examples/range-over-channels/range-over-channels.sh
+++ examples/range-over-channels/range-over-channels.sh
@@ -2,6 +2,4 @@ $ go run range-over-channels.go
 one
 two
 
-# This example also showed that it's possible to close
-# a non-empty channel but still have the remaining
-# values be received.
+# 이 예제 또한 비어있지 않은 채널을 닫아도 채널에 남아있는 값들은 수신할 수 있다는걸 보여주고 있습니다.

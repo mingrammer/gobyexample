a87e25778a4d0e785426f5cb620f163f9d975bc8
mingrammer <k239507@gmail.com>
2017-01-23 15:07:29 +0900

    Translate the sorting

--- examples/sorting/sorting.go
+++ examples/sorting/sorting.go
@@ -1,6 +1,5 @@
-// Go's `sort` package implements sorting for builtins
-// and user-defined types. We'll look at sorting for
-// builtins first.
+// Go의 `sort` 패키지는 내장 타입과 사용자 정의 타입을 위한 정렬을 구현하고 있습니다.
+//  먼저 내장 타입에 대한 정렬을 살펴봅시다.
 
 package main
 
@@ -9,21 +8,18 @@ import "sort"
 
 func main() {
 
-	// Sort methods are specific to the builtin type;
-	// here's an example for strings. Note that sorting is
-	// in-place, so it changes the given slice and doesn't
-	// return a new one.
+	// 정렬 메서드는 내장 타입에만 해당합니다. 다음은 문자열에 대한 예시입니다.
+	//  참고로 정렬은 제자리 정렬(in-place)이므로, 이는 전달된 슬라이스를 변형시키며 새로운 슬라이스를 반환하진 않습니다.
 	strs := []string{"c", "a", "b"}
 	sort.Strings(strs)
 	fmt.Println("Strings:", strs)
 
-	// An example of sorting `int`s.
+	// `int`형의 정렬 예시입니다.
 	ints := []int{7, 2, 4}
 	sort.Ints(ints)
 	fmt.Println("Ints:   ", ints)
 
-	// We can also use `sort` to check if a slice is
-	// already in sorted order.
+	// 또한 `sort`를 사용해 슬라이스가 이미 정렬되어 있는지에 대한 여부도 확인할 수 있습니다.
 	s := sort.IntsAreSorted(ints)
 	fmt.Println("Sorted: ", s)
 }
--- examples/sorting/sorting.sh
+++ examples/sorting/sorting.sh
@@ -1,5 +1,4 @@
-# Running our program prints the sorted string and int
-# slices and `true` as the result of our `AreSorted` test.
+# 프로그램을 실행하면 정렬된 문자열과 정수 슬라이스 그리고 `AreSorted` 테스트의 결과로 `true`가 출력됩니다.
 $ go run sorting.go
 Strings: [a b c]
 Ints:    [2 4 7]

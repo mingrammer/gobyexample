b29f4b816af0cbdf73274d7ab1b7f5d6f21ed3cf
mingrammer <k239507@gmail.com>
2017-01-27 23:21:49 +0900

    Translate the string-functions

--- examples/string-functions/string-functions.go
+++ examples/string-functions/string-functions.go
@@ -1,25 +1,19 @@
-// The standard library's `strings` package provides many
-// useful string-related functions. Here are some examples
-// to give you a sense of the package.
+// 표준 라이브러리의 `strings` 패키지는 많은 유용한 문자열 관련 함수들을 제공합니다.
+//  이 패키지에 대한 감을 얻을 수 있는 몇 가지 예시가 있습니다.
 
 package main
 
 import s "strings"
 import "fmt"
 
-// We alias `fmt.Println` to a shorter name as we'll use
-// it a lot below.
+// `fmt.Println`를 짧은 이름으로 aliasing 합니다. 이는 아래에서 많이 사용합니다.
 var p = fmt.Println
 
 func main() {
 
-	// Here's a sample of the functions available in
-	// `strings`. Since these are functions from the
-	// package, not methods on the string object itself,
-	// we need pass the string in question as the first
-	// argument to the function. You can find more
-	// functions in the [`strings`](http://golang.org/pkg/strings/)
-	// package docs.
+	// `strings`에서 사용할 수 있는 몇 가지 함수의 예시입니다.
+	//  이들은 문자열 객체 자체의 메서드가 아니라 패키지의 함수이기 때문에 문자열을 함수의 첫번째 인자에 전달해줘야 합니다.
+	//  [`strings`](http://golang.org/pkg/strings/) 패키지 문서에서 더 많은 함수들을 찾아볼 수 있습니다.
 	p("Contains:  ", s.Contains("test", "es"))
 	p("Count:     ", s.Count("test", "t"))
 	p("HasPrefix: ", s.HasPrefix("test", "te"))
@@ -34,16 +28,12 @@ func main() {
 	p("ToUpper:   ", s.ToUpper("test"))
 	p()
 
-	// Not part of `strings`, but worth mentioning here, are
-	// the mechanisms for getting the length of a string in
-	// bytes and getting a byte by index.
+	// `strings`의 일부는 아니지만 언급할만한게 있는데, 바이트에서의 문자열 길이와 인덱스를 통한 바이트를 구하는 메커니즘입니다.
 	p("Len: ", len("hello"))
 	p("Char:", "hello"[1])
 }
 
-// Note that `len` and indexing above work at the byte level.
-// Go uses UTF-8 encoded strings, so this is often useful
-// as-is. If you're working with potentially multi-byte
-// characters you'll want to use encoding-aware operations.
-// See [strings, bytes, runes and characters in Go](https://blog.golang.org/strings)
-// for more information.
+// 참고로 위의 `len`과 인덱싱은 바이트 수준에서 동작합니다.
+//  Go는 UTF-8로 인코딩된 문자열을 사용하므로 종종 유용합니다.
+//  잠재적으로 다중 바이트 바이트 문자로 작업하는 경우 인코딩 인식 작업을 하고자 할겁니다.
+//  좀 더 자세한 내용은 [strings, bytes, runes and characters in Go](https://blog.golang.org/strings)를 보십시오.

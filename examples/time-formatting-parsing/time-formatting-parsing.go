3204ff912be5ce1f8dc445b8b7dead79f5620d0c
mingrammer <k239507@gmail.com>
2017-01-28 14:17:10 +0900

    Translate the time-formatting-parsing

--- examples/time-formatting-parsing/time-formatting-parsing.go
+++ examples/time-formatting-parsing/time-formatting-parsing.go
@@ -1,5 +1,4 @@
-// Go supports time formatting and parsing via
-// pattern-based layouts.
+// Go는 패턴 기반의 레이아웃을 통한 시간 포맷팅과 파싱을 지원합니다.
 
 package main
 
@@ -9,25 +8,19 @@ import "time"
 func main() {
 	p := fmt.Println
 
-	// Here's a basic example of formatting a time
-	// according to RFC3339, using the corresponding layout
-	// constant.
+	// 다음은 RFC3339에 해당하는 레이아웃 상수를 사용하여 시간을 포맷팅하는 기본적인 예시입니다.
 	t := time.Now()
 	p(t.Format(time.RFC3339))
 
-	// Time parsing uses the same layout values as `Format`.
+	// 시간 파싱은 `Format`과 동일한 레이아웃 값을 사용합니다.
 	t1, e := time.Parse(
 		time.RFC3339,
 		"2012-11-01T22:08:41+00:00")
 	p(t1)
 
-	// `Format` and `Parse` use example-based layouts. Usually
-	// you'll use a constant from `time` for these layouts, but
-	// you can also supply custom layouts. Layouts must use the
-	// reference time `Mon Jan 2 15:04:05 MST 2006` to show the
-	// pattern with which to format/parse a given time/string.
-	// The example time must be exactly as shown: the year 2006,
-	// 15 for the hour, Monday for the day of the week, etc.
+	// `Format`과 `Parse`는 예시 기반 레이아웃을 사용합니다. 보통 이 레이아웃들은 `time`에서의 상수를 사용하지만, 커스텀 레이아웃을 사용할 수도 있습니다.
+	//  레이아웃은 참조 시간 `Mon Jan 2 15:04:05 MST 2006`을 사용하여 주어진 시간/문자열을 포맷팅/파싱할 패턴을 표시해야합니다.
+	//  예시 시간은 다음에 표시된것과 정확히 일치해야합니다: 연도는 2006, 시간은 15, 요일은 월요일 등등.
 	p(t.Format("3:04PM"))
 	p(t.Format("Mon Jan _2 15:04:05 2006"))
 	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
@@ -35,15 +28,12 @@ func main() {
 	t2, e := time.Parse(form, "8 41 PM")
 	p(t2)
 
-	// For purely numeric representations you can also
-	// use standard string formatting with the extracted
-	// components of the time value.
+	// 순전히 숫자로 표현하기 위해 시간값에서 추출한 컴포넌트와 표준 문자열 포맷팅을 사용할 수도 있습니다.
 	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
 		t.Year(), t.Month(), t.Day(),
 		t.Hour(), t.Minute(), t.Second())
 
-	// `Parse` will return an error on malformed input
-	// explaining the parsing problem.
+	// `Parse`는 잘못된 형식의 입력값이 들어오면 에러를 반환하여 파싱 문제를 설명합니다.
 	ansic := "Mon Jan _2 15:04:05 2006"
 	_, e = time.Parse(ansic, "8:41PM")
 	p(e)

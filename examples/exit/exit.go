a3c364f5ad481a1e62fd0be1b49be89da12ba9f6
Seokju Hong <tjkj555@gmail.com>
2017-01-29 22:32:09 +0900

    Translated exit chapter

--- examples/exit/exit.go
+++ examples/exit/exit.go
@@ -1,5 +1,4 @@
-// Use `os.Exit` to immediately exit with a given
-// status.
+// `os.Exit`를 이용하여 프로그램을 지정된 status로 즉시 종료할 수 있습니다.
 
 package main
 
@@ -8,15 +7,14 @@ import "os"
 
 func main() {
 
-	// `defer`s will _not_ be run when using `os.Exit`, so
-	// this `fmt.Println` will never be called.
+  // `defer`는 `os.Exit`를 이용할 때에는 _작동하지 않습니다._
+  // 그래서 이 `fmt.Println`은 절대로 호출되지 않을 것입니다.
 	defer fmt.Println("!")
 
-	// Exit with status 3.
+  // status 3으로 종료해봅시다.
 	os.Exit(3)
 }
 
-// Note that unlike e.g. C, Go does not use an integer
-// return value from `main` to indicate exit status. If
-// you'd like to exit with a non-zero status you should
-// use `os.Exit`.
+// C와 같은 다른 언어와는 달리, Go는 main에서 return된 값으로
+//  exit status를 나타내지 않습니다. 만약 0이 아닌 다른 status로
+//  종료하고 싶다면 `os.Exit`를 이용해야 합니다.

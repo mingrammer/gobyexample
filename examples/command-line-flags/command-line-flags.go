7e62d3eaa09b8819b572676d10c3b9013652bd67
mingrammer <k239507@gmail.com>
2017-01-29 20:28:19 +0900

    Translate the command-line-flags

--- examples/command-line-flags/command-line-flags.go
+++ examples/command-line-flags/command-line-flags.go
@@ -1,46 +1,34 @@
-// [_Command-line flags_](http://en.wikipedia.org/wiki/Command-line_interface#Command-line_option)
-// are a common way to specify options for command-line
-// programs. For example, in `wc -l` the `-l` is a
-// command-line flag.
+// [_커맨드라인 플래그 (Command-line flags)_](http://en.wikipedia.org/wiki/Command-line_interface#Command-line_option)는 커맨드라인 프로그램에 옵션을 지정하는 일반적인 방법입니다.
+//  예를 들어, `wc -l`에서 `-l`은 커맨드라인 플래그입니다.
 
 package main
 
-// Go provides a `flag` package supporting basic
-// command-line flag parsing. We'll use this package to
-// implement our example command-line program.
+// Go는 기본적인 커맨드라인 플래그 파싱을 지원하는 `flag` 패키지를 제공합니다.
+//  이 패키지를 사용하여 예제 커맨드라인 프로그램을 구현해보겠습니다.
 import "flag"
 import "fmt"
 
 func main() {
 
-	// Basic flag declarations are available for string,
-	// integer, and boolean options. Here we declare a
-	// string flag `word` with a default value `"foo"`
-	// and a short description. This `flag.String` function
-	// returns a string pointer (not a string value);
-	// we'll see how to use this pointer below.
+	// 기본적인 플래그 선언은 문자열, 정수, 그리고 불리언 옵션에서 사용 가능합니다. 다음은 `"foo"`를 기본값으로 하고 간단한 설명을 가진 `word`라는 문자열 플래그의 선언입니다.
+	//  `flag.String` 함수는 문자열 포인터를 반환합니다 (문자열 값이 아님)
+	//  아래에서 이 포인터를 어떻게 사용하는지 살펴보겠습니다.
 	wordPtr := flag.String("word", "foo", "a string")
 
-	// This declares `numb` and `fork` flags, using a
-	// similar approach to the `word` flag.
+	// 다음은 `word` 플래그 선언과 유사한 방법을 사용해 선언한 `numb`와 `fork` 플래그입니다.
 	numbPtr := flag.Int("numb", 42, "an int")
 	boolPtr := flag.Bool("fork", false, "a bool")
 
-	// It's also possible to declare an option that uses an
-	// existing var declared elsewhere in the program.
-	// Note that we need to pass in a pointer to the flag
-	// declaration function.
+	// 프로그램의 기존에 다른 곳에서 선언된 변수를 사용해 옵션을 선언하는 것도 가능합니다.
+	//  참고로 플래그 선언 함수에 변수의 포인터를 전달해야합니다.
 	var svar string
 	flag.StringVar(&svar, "svar", "bar", "a string var")
 
-	// Once all flags are declared, call `flag.Parse()`
-	// to execute the command-line parsing.
+	// 모든 플래그가 선언되면, 커맨드라인 파싱을 실행하기 위해 `flag.Parse()`을 호출합니다.
 	flag.Parse()
 
-	// Here we'll just dump out the parsed options and
-	// any trailing positional arguments. Note that we
-	// need to dereference the pointers with e.g. `*wordPtr`
-	// to get the actual option values.
+	// 다음은 파싱된 옵션들과 후미에 위치하는 인자들을 덤프합니다.
+	//  참고로 실제 옵션값을 얻기 위해선 `*wordPtr`와 같이 포인터를 역참조 해야합니다.
 	fmt.Println("word:", *wordPtr)
 	fmt.Println("numb:", *numbPtr)
 	fmt.Println("fork:", *boolPtr)
--- examples/command-line-flags/command-line-flags.sh
+++ examples/command-line-flags/command-line-flags.sh
@@ -1,10 +1,7 @@
-# To experiment with the command-line flags program it's
-# best to first compile it and then run the resulting
-# binary directly.
+# 커맨드라인 플래그 프로그램을 실험하려면 우선 이를 컴파일한 후 직접 결과 바이너리를 실행하는 것이 가장 좋습니다.
 $ go build command-line-flags.go
 
-# Try out the built program by first giving it values for
-# all flags.
+# 처음엔 모든 플래그에 값을 주어 프로그램을 실행해 봅시다.
 $ ./command-line-flags -word=opt -numb=7 -fork -svar=flag
 word: opt
 numb: 7
@@ -12,8 +9,7 @@ fork: true
 svar: flag
 tail: []
 
-# Note that if you omit flags they automatically take
-# their default values.
+# 참고로 생략된 플래그들은 자동으로 기본값을 갖게 됩니다.
 $ ./command-line-flags -word=opt
 word: opt
 numb: 42
@@ -21,16 +17,13 @@ fork: false
 svar: bar
 tail: []
 
-# Trailing positional arguments can be provided after
-# any flags.
+# 후미에 위치한 인자들은 모든 플래그 뒤에 위치할 수 있습니다.
 $ ./command-line-flags -word=opt a1 a2 a3
 word: opt
 ...
 tail: [a1 a2 a3]
 
-# Note that the `flag` package requires all flags to
-# appear before positional arguments (otherwise the flags
-# will be interpreted as positional arguments).
+# 참고로 `flag` 패키지에서 모든 플래그들은 위치 인자 전에 나타나야합니다. (그렇지 않으면 플래그들은 위치 인자로 해석됩니다)
 $ ./command-line-flags -word=opt a1 a2 a3 -numb=7
 word: opt
 numb: 42
@@ -38,8 +31,7 @@ fork: false
 svar: bar
 tail: [a1 a2 a3 -numb=7]
 
-# Use `-h` or `--help` flags to get automatically
-# generated help text for the command-line program.
+# `-h` 또는 `--help` 플래그를 사용하면 커맨드라인 프로그램에 대해 자동으로 생성된 도움말을 볼 수 있습니다. 
 $ ./command-line-flags -h
 Usage of ./command-line-flags:
   -fork=false: a bool
@@ -47,13 +39,10 @@ Usage of ./command-line-flags:
   -svar="bar": a string var
   -word="foo": a string
 
-# If you provide a flag that wasn't specified to the
-# `flag` package, the program will print an error message
-# and show the help text again.
+# `flag` 패키지로 지정되지 않은 플래그를 사용하면, 프로그램은 에러 메시지를 출력하며 도움말을 다시 보여줍니다.
 $ ./command-line-flags -wat
 flag provided but not defined: -wat
 Usage of ./command-line-flags:
 ...
 
-# Next we'll look at environment variables, another common
-# way to parameterize programs.
+# 다음장에선 또 다른 프로그램을 매개변수화하는 일반적인 방법인 환경변수에 대해 살펴보겠습니다.

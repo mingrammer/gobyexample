cbaefee1688c88ec158cef5053f715f29f54aa01
mingrammer <k239507@gmail.com>
2017-01-29 19:11:47 +0900

    Translate the line-filters

--- examples/line-filters/line-filters.go
+++ examples/line-filters/line-filters.go
@@ -1,11 +1,8 @@
-// A _line filter_ is a common type of program that reads
-// input on stdin, processes it, and then prints some
-// derived result to stdout. `grep` and `sed` are common
-// line filters.
+// _라인 필터 (line filter)_는 stdin으로 입력을 읽고, 처리한 후, 나온 결괏값을 stdout으로 출력하는 일반적인 유형의 프로그램입니다.
+//  `grep`과 `sed`가 일반적인 라인 필터입니다.
 
-// Here's an example line filter in Go that writes a
-// capitalized version of all input text. You can use this
-// pattern to write your own Go line filters.
+// 다음은 모든 입력 텍스트의 대문자 버전을 작성하는 Go의 라인 필터 예제입니다.
+//  여러분만의 Go 라인 필터를 작성하는데 이 패턴을 사용할 수 있습니다.
 package main
 
 import (
@@ -17,23 +14,19 @@ import (
 
 func main() {
 
-	// Wrapping the unbuffered `os.Stdin` with a buffered
-	// scanner gives us a convenient `Scan` method that
-	// advances the scanner to the next token; which is
-	// the next line in the default scanner.
+	// 버퍼링되지 않은 `os.Stdin`을 버퍼링된 스캐너로 래핑하는건 스캐너를 다음 토큰으로 진행시키는 편리한 `Scan` 메서드를 제공합니다.
+	//  기본 스캐너에서 다음 토큰은 다음 라인입니다.
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-		// `Text` returns the current token, here the next line,
-		// from the input.
+		// `Text`는 입력에서 현재 토큰을 반환합니다. 여기선 다음 라인입니다.
 		ucl := strings.ToUpper(scanner.Text())
 
-		// Write out the uppercased line.
+		// 대문자로된 문자열을 라인에 씁니다.
 		fmt.Println(ucl)
 	}
 
-	// Check for errors during `Scan`. End of file is
-	// expected and not reported by `Scan` as an error.
+	// `Scan`중 에러를 체크합니다. 파일의 끝이 예상되며 `Scan`에 의한 에러로 보고되지 않습니다.
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
--- examples/line-filters/line-filters.sh
+++ examples/line-filters/line-filters.sh
@@ -1,9 +1,8 @@
-# To try out our line filter, first make a file with a few
-# lowercase lines.
+# 라인 필터를 실행해 보기위해, 몇 개의 소문자 라인들을 가진 파일을 생성합니다.
 $ echo 'hello'   > /tmp/lines
 $ echo 'filter' >> /tmp/lines
 
-# Then use the line filter to get uppercase lines.
+# 다음으로 대문자 라인들을 얻기 위해 라인 필터를 사용합니다.
 $ cat /tmp/lines | go run line-filters.go
 HELLO
 FILTER

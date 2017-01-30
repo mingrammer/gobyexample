209e276a7738d33bc1a0d1cc73578e63a1204e89
ming <k239507@gmail.com>
2017-01-30 23:32:36 +0900

    Update execing-processes.sh

--- examples/execing-processes/execing-processes.sh
+++ examples/execing-processes/execing-processes.sh
@@ -6,6 +6,6 @@ drwxr-xr-x 91 mark 3.0K Oct 3 12:50 ..
 -rw-r--r--  1 mark 1.3K Oct 3 16:28 execing-processes.go
 
 # Go가 고전 Unix의 `fork`를 제공하지 않는다는 사실을 기억하십시오.
-#  보통 Goroutine을 이용하여 프로세스를 생성하거나 `exec`하여
+#  보통 고루틴을 이용하여 프로세스를 생성하거나 `exec`하여
 #  `fork`의 대부분의 use case를 다루기 때문에
 #  웬만해선 문제가 되지 않을 것입니다.
